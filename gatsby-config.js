af0e9e8af825e72e8f6ba81a5ccd4347dd9551db
Bdrxzzzz <75257533+badriian24@users.noreply.github.com>
2021-06-17 10:58:41 +0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Bdrxzzz',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Bdrxzzz is a developers unknow ehe.',
+    siteUrl: 'https://bdrxzzz.ml', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@fiqih_badrian',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'Bdrxzzz',
+        short_name: 'FiqihBadrian',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
